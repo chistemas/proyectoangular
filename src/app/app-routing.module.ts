a6989f33e3de984d47d24d443dba9a89fc4933ae
fernandomf250 <64025684+fernandomf250@users.noreply.github.com>
2020-08-04 09:25:43 -0500

    version1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { TiendaComponent} from './components/tienda/tienda.component';
+import { ContactanosComponent} from './components/contactanos/contactanos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: InicioComponent},
+  { path: 'tienda', component: TiendaComponent},
+  { path: 'contactanos', component: ContactanosComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TiendaComponent } from './components/tienda/tienda.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TiendaComponent,
+    InicioComponent,
+    ContactanosComponent
   ],
   imports: [
     BrowserModule,
